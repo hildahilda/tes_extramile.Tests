23c6d2086cf64efe8c73bc452fb79fc873dc7814
hildahilda <hilda@hilda.com>
2021-10-16 19:25:34 +0800

    adding tes_extramile.Tests

---
+++ UnitTest1.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using tes_extramile.Models;
+using tes_extramile.api.Controllers;
+
+namespace tes_extramile.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void serviceLevel()
+        {
+            var helper = new WeatherApiController();
+        }
+    }
+}
